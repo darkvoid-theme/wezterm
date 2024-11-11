65a9c41a5645001830c477be093186a406bf9498
Aliqyan-21 <aliqyanabid21@gmail.com>
2024-11-11 23:27:39 +0530

    feat: wezterm darkvoid colors

---
+++ wezterm.lua
@@ -0,0 +1,53 @@
+return {
+	colors = {
+		-- darkvoid color scheme base
+		foreground = "#c0c0c0",
+		background = "#040709",
+
+		cursor_bg = "#c1c1c1",
+		cursor_fg = "#090a04",
+		cursor_border = "#7fbfff",
+		selection_fg = "#c0c0c0",
+		selection_bg = "#303030",
+		scrollbar_thumb = "#404040",
+
+		ansi = {
+			"#1c1c1c",
+			"#ff3131",
+			"#66b2b2",
+			"#d1d1d1",
+			"#4b8902",
+			"#b16286",
+			"#1bfd9c",
+			"#c0c0c0",
+		},
+
+		brights = {
+			"#404040",
+			"#fb4934",
+			"#b8bb26",
+			"#9efd84",
+			"#83a598",
+			"#d3869b",
+			"#8ec07c",
+			"#ebdbb2",
+		},
+
+		visual_bell = "#7fbfff",
+		tab_bar = {
+			background = "#1c1c1c",
+			active_tab = {
+				bg_color = "#303030",
+				fg_color = "#c0c0c0",
+			},
+			inactive_tab = {
+				bg_color = "#1c1c1c",
+				fg_color = "#585858",
+			},
+			inactive_tab_hover = {
+				bg_color = "#303030",
+				fg_color = "#c0c0c0",
+			},
+		},
+	},
+}
